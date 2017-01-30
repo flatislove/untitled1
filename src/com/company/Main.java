a4951d98e52ce0510f88abff8f7b826eef539271
flatislove <renadiz11ak>
2017-01-30 20:51:34 +0300

    jjjjjl

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,10 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("kkkkkk");
+        System.out.println("oooooo");
+    }
+
+    public int uu(){
+        return 9;
     }
 }
